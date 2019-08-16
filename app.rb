208c806fff0b945a17d73f8f7f0d1c57e1b42d06
soken <bknb.hy.lav@gmail.com>
2019-08-16 16:48:11 +0900

    最初のコミット

--- app.rb
+++ app.rb
@@ -30,9 +30,8 @@
 end
 
   get '/index' do
-    active_user = session[:user_id]
-    @posts = db.exec_params("SELECT * FROM posts")
-    
+    @images = db.exec_params('select * from images')
+
     erb :index
   end
 
@@ -41,14 +40,16 @@ end
   end
 
   post '/post' do
-    active_user = session[:user_id]
-    content = params[:content]
     name = params[:name]
-    db.exec_params("INSERT INTO posts(name,content) VALUES($1,$2)",[name,content])
-   
+    content = params[:content]
+    image = params[:img][:filename]
+    @file_name = params[:img][:filename]
+    FileUtils.mv(params[:img][:tempfile], "./public/images/#{@file_name}")
+    db.exec_params("INSERT INTO images(name,content,image) VALUES($1,$2,$3)",[name,content,image])
     redirect '/index'
   end
 
+  
   get '/sign_up' do
     erb :sign_up
   end
@@ -62,7 +63,7 @@ end
       db.exec_params("INSERT INTO users(name,email,password) VALUES($1,$2,$3)",[name,email,password])
       redirect '/sign_in'
     else
-      redirect '/sign_up'
+      redirect '/already_sign_up'
   end
 end
 
@@ -81,101 +82,14 @@ end
       session[:user_id] = id['id']
       redirect '/index'
     else
-      redirect '/'
+      redirect '/yet_sign_up'
     end
   end
-
-  
-
-  
-
-
-
-
-
-#   #Sinatraの設定
-#   set :public_folder, 'public'
-#   enable :sessions
-
-  
-
-#   get '/hello' do
-#     # query string から取得
-#     # @name = params[:name]
-#     session[:name] =  params[:name]
-
-#     erb :hello
-# end
-
-  
-
-
-
-#   get '/read' do
-#     if session[:name].nil? 
-#       "<h1>あなたは誰ですか？</h1>"
-#     else
-#       "<h1>ようこそ#{session[:name]}</h1>"
-#     end
-#   end
-  
-
-#   get '/user/:user_name' do 
-#     user_name = params[:user_name]
-#     "<h1>User: #{user_name}!</h1>"
-#   end
-
-
-
-#   get '/time' do
-#     Time.now.to_s
-#   end
-
-#   get '/form' do
-#     erb :form
-#   end
-
-#   post '/form_output' do
-#     @name = params[:name]
-#     @email = params[:email]
-#     @content = params[:content]
-
-# #   File.open("form.txt", mode = "a") {|f|
-# #     f.write("#{@name},#{@email},#{@content}\n")
-# #   }
  
-# #     erb:form_output
-# # end
+  get '/already_sign_up' do
+    erb :already_sign_up
+  end
 
-# #   get '/upload' do
-# #     @images = Dir.glob("./public/images/*").map{|path| path.split('/').last }
-# #    erb :upload
-# #   end
- 
-
-# #   post '/upload' do
-# #     @file_name = params[:img][:filename]
-# #     FileUtils.mv(params[:img][:tempfile], "./public/images/#{@file_name}")
-# #     # erb :uploaded
-# #     redirect '/upload'
-# #    end
-  
-   
-
-   
-  
-
-  
-
-  
-
-  
-#   # get '/form' do
-#   #   erb :form
-#   # end
-
-
-
-
-
-
+  get '/yet_sign_up' do
+    erb :yet_sign_up
+  end
